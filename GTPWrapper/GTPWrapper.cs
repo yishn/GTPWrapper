e9d2811a6ad7c79c83665e431f75e9f1d791c578
yishn <shen.yichuan@gmail.com>
2014-10-19 21:17:56 +0200

    Adding preliminaries

---
+++ GTPWrapper/GTPWrapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTPWrapper {
+	public class GTPWrapper {
+		public GTPWrapper() {
+
+		}
+	}
+}
