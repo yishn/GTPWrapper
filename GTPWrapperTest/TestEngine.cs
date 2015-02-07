772370344ceeda3b7a1e49d9df5496011f6b8280
yishn <shen.yichuan@gmail.com>
2015-02-07 23:16:34 +0100

    Update GTPWrapperTest

---
+++ GTPWrapperTest/TestEngine.cs
@@ -0,0 +1,26 @@
+﻿using GTPWrapper;
+using GTPWrapper.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTPWrapperTest {
+    public class TestEngine : Engine {
+        public TestEngine() : base("GTP Test Engine", "1.0") {
+            this.SupportedCommands.AddRange(new string[] { "showboard", "error" });
+        }
+
+        protected override Response ExecuteCommand(Command command) {
+            switch (command.Name) {
+                case "error":
+                    return new Response(command, "an expected error occurred", true);
+                case "showboard":
+                    return new Response(command, new Board(19).ToString());
+            }
+
+            return base.ExecuteCommand(command);
+        }
+    }
+}
