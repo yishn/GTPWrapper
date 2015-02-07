d8673b13ab65cf3a708fdfa5b1e347038ca2e9ad
yishn <shen.yichuan@gmail.com>
2015-02-07 22:00:43 +0100

    Add xmldocs

--- GTPWrapper/Engine.cs
+++ GTPWrapper/Engine.cs
@@ -36,7 +36,13 @@ namespace GTPWrapper {
         /// </summary>
         public Dictionary<Command, Response> ResponseList;
 
+        /// <summary>
+        /// Gets or sets the name of the engine.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the version of the engine.
+        /// </summary>
         public string Version { get; set; }
 
         /// <summary>
