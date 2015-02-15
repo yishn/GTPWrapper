2352ebad3066cd3b9e22df1e9f391362ba165f4e
yishn <shen.yichuan@gmail.com>
2015-02-15 21:18:36 +0100

    Rename

---
+++ GTPWrapper/Sgf/SgfParseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTPWrapper.Sgf {
+    /// <summary>
+    /// The exception that is thrown when there is an error in the parsing process of a SGF string.
+    /// </summary>
+    public class SgfParseException : Exception {
+        public SgfParseException(string message = "") : base(message) { }
+    }
+}
