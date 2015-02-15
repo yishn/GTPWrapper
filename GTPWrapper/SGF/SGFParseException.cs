2e1c26fd5950975475287a783823b461ef784d8a
yishn <shen.yichuan@gmail.com>
2015-02-15 20:40:50 +0100

    Add SGF Tokenizer

---
+++ GTPWrapper/SGF/SGFParseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GTPWrapper.SGF {
+    /// <summary>
+    /// The exception that is thrown when there is an error in the parsing process of a SGF string.
+    /// </summary>
+    public class SGFParseException : Exception {
+        public SGFParseException(string message = "") : base(message) { }
+    }
+}
