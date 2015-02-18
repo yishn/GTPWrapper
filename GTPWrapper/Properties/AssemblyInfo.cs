c6cc2bba393f59b5c97f28e433e702eaf399c7cd
Yichuan Shen <yishn@users.noreply.github.com>
2015-02-18 01:26:37 +0100

    Update version, description and copyright

--- GTPWrapper/Properties/AssemblyInfo.cs
+++ GTPWrapper/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
 [assembly: AssemblyTitle("GTPWrapper")]
-[assembly: AssemblyDescription("A GTP wrapper for C#")]
+[assembly: AssemblyDescription("A Go Text Protocol engine library for C#")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GTPWrapper")]
-[assembly: AssemblyCopyright("Copyright © Yichuan Shen 2014")]
+[assembly: AssemblyCopyright("Copyright © Yichuan Shen 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
