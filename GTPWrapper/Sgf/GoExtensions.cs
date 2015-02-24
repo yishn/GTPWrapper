2eef75d4ec3364b21191ebba02e8f7436146e1cf
yishn <shen.yichuan@gmail.com>
2015-02-24 22:42:23 +0100

    Consider pass vertices

--- GTPWrapper/Sgf/GoExtensions.cs
+++ GTPWrapper/Sgf/GoExtensions.cs
@@ -8,10 +8,12 @@ using System.Threading.Tasks;
 namespace GTPWrapper.Sgf {
     public static class GoExtensions {
         public static string VertexToSgf(this Board board, Vertex vertex) {
+            if (vertex == Vertex.Pass || !board.HasVertex(vertex)) return "";
             return (Vertex.Letters[vertex.X - 1].ToString() + Vertex.Letters[board.Size - vertex.Y - 1].ToString()).ToLower();
         }
 
         public static Vertex SgfToVertex(this Board board, string sgfVertex) {
+            if (sgfVertex == "" || board.Size <= 19 && sgfVertex == "tt") return Vertex.Pass;
             return new Vertex(Vertex.Letters.IndexOf(sgfVertex[0]) + 1, board.Size - Vertex.Letters.IndexOf(sgfVertex[1]));
         }
     }
