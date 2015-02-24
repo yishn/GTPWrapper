fe744632e8e79b664ffccee8a7388ace5e739230
yishn <shen.yichuan@gmail.com>
2015-02-24 23:34:07 +0100

    Add docs

--- GTPWrapper/Sgf/GoExtensions.cs
+++ GTPWrapper/Sgf/GoExtensions.cs
@@ -7,11 +7,21 @@ using System.Threading.Tasks;
 
 namespace GTPWrapper.Sgf {
     public static class GoExtensions {
+        /// <summary>
+        /// Converts given vertex on given board into SGF coordinates.
+        /// </summary>
+        /// <param name="board">The board.</param>
+        /// <param name="vertex">The vertex.</param>
         public static string VertexToSgf(this Board board, Vertex vertex) {
             if (vertex == Vertex.Pass || !board.HasVertex(vertex)) return "";
             return (Vertex.Letters[vertex.X - 1].ToString() + Vertex.Letters[board.Size - vertex.Y - 1].ToString()).ToLower();
         }
 
+        /// <summary>
+        /// Converts given SGF coordinates on given board into a vertex.
+        /// </summary>
+        /// <param name="board">The board.</param>
+        /// <param name="sgfVertex">The SGF coordinates.</param>
         public static Vertex SgfToVertex(this Board board, string sgfVertex) {
             if (sgfVertex == "" || board.Size <= 19 && sgfVertex == "tt") return Vertex.Pass;
             return new Vertex(
